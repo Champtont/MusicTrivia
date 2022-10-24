52f0e322b5822f2068a34d42387f1acbf9d279ca
Courtney Hampton <jaye0888@gmail.com>
2022-10-24 12:54:40 +0200

    true and false being read
    Now JS understands which answers are correct and which answers are false... this will help JS keep score. Also adjusted CSS

--- questions.js
+++ questions.js
@@ -1,27 +1,80 @@
 //Let's grab things that we will need
 const questionContainer = document.getElementById("container");
 const questionele = document.getElementById("question");
-const answerButtons = document.querySelectorAll(".option");
+const pText = document.querySelectorAll(".option");
+const answerButtons = document.querySelectorAll(".stepford");
 
+console.log(answerButtons);
 //we need questions to work with! An Array of objects is required.
 const questionsArray = [
   {
-    question: "(test question)What is two plus two?",
+    question:
+      " What was the last single released from Michael Jackson’s Thriller album?",
     answers: [
-      { text: "four", correct: true },
-      { text: "22", correct: false },
-      { text: "3", correct: false },
-      { text: "5", correct: false },
+      { text: "Thriller", correct: true },
+      { text: "Billie Jean", correct: false },
+      { text: "Beat It", correct: false },
+      { text: "Human Nature", correct: false },
     ],
   },
   {
-    question: "does this work?",
+    question: "What song is considered to be Madonna’s first mainstream hit?",
     answers: [
-      { text: "I hope so", correct: false },
-      { text: "Hell yes!", correct: true },
+      { text: "Holiday", correct: true },
+      { text: "Isla Bonita", correct: false },
+      { text: "Vogue", correct: false },
+      { text: "Papa Don't Preach", correct: false },
+    ],
+  },
+  {
+    question: "What movie was Eye of the Tiger the theme song for?",
+    answers: [
+      { text: "Rocky III", correct: true },
+      { text: "Rocky II", correct: false },
+      { text: "Rocky I", correct: false },
+      { text: "Rocky IV", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who is the band that reminded listeners to don’t stop believing?",
+    answers: [
+      { text: "Journey", correct: true },
+      { text: "Aerosmith", correct: false },
+      { text: " The Rolling Stones", correct: false },
+      { text: "Def Lepard", correct: false },
+    ],
+  },
+  {
+    question: "Who was the hype man in the rap group Public Enemy?",
+    answers: [
+      { text: "Flavor Flav", correct: true },
+      { text: "E-Love", correct: false },
+      { text: "The Bosstone", correct: false },
+      { text: "Jay-Z", correct: false },
+    ],
+  },
+  {
+    question:
+      "Who is the 80’s rock band known for the hits “Livin’ on a Prayer” and “You Give Love a Bad Name?",
+    answers: [
+      { text: "Bon Jovi", correct: true },
+      { text: "Aerosmith", correct: false },
+      { text: "Guns N Roses", correct: false },
+      { text: "Journey", correct: false },
     ],
   },
 ];
+
+//These may change
+let score = 0;
+let maxQuestions = questionsArray.length;
+//lets display these values
+const pointInput = document.getElementById("pointsScored");
+const maxQInput = document.getElementById("maxPoints");
+
+pointInput.value = score;
+maxQInput.value = maxQuestions;
 
 //shuffling questions
 const shuffleQuestions = questionsArray.sort(() => Math.random() - 0.5);
@@ -37,13 +90,27 @@ const showQuestion = (question) => {
   //randomizing location of answers
   const answers = question.answers.sort(() => Math.random() - 0.5);
 
-  //below accesses first index then the text value
-  console.log(answers[0]["text"]);
+  //below accesses first index then the text value...testing
+  // console.log(answers[0]["text"]);
+
   //run for loop that places the text into each container
   for (let i = 0; i < answers.length; i++) {
-    answerButtons[i].innerText = answers[i]["text"];
+    pText[i].innerText = answers[i]["text"];
+    //and then whether the answer is correct or not.
+    pText[i].answers = answers[i]["correct"];
+
+    //add button Event listener to all buttons
+    answerButtons[i].addEventListener("click", selectAnswer);
   }
 };
+
+//create select answer event
+function selectAnswer(event) {
+  //gives info on button clicked
+  const selectedbutton = event.target;
+  const correct = selectedbutton.answers;
+  console.log(correct);
+}
 
 //creating a set question function:
 const setNextQuestion = () => {
@@ -52,3 +119,10 @@ const setNextQuestion = () => {
 
 //Then calling inittial function: This will display immediately:
 setNextQuestion();
+
+//try to create a correct answer data set This should check answer inside next button
+/*if (answers.correct === true) {
+  score++;
+} else {
+  score--;
+}*/
