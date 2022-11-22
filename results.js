efa95831abc4bfef130db4748d5667b27852b97c
Courtney Hampton <jaye0888@gmail.com>
2022-11-22 19:16:22 +0100

    Results are calculating
    Want to come up with a few more questions eventually... but it's working!

--- results.js
+++ results.js
@@ -1,3 +1,20 @@
 const url = new URLSearchParams(window.location.search);
 const userScore = url.get("score");
 const maxPoints = url.get("total");
+const percentage = (userScore / maxPoints) * 100;
+
+console.log(percentage);
+const resultBar = document.getElementById("results-barTop");
+resultBar.style.backgroundImage = `linear-gradient(90deg, gold ${percentage}%, transparent ${percentage}%)`;
+
+//give them the score
+const h2 = document.querySelector("h2");
+h2.innerText = `You Scored: ${userScore} out of ${maxPoints}`;
+
+//let them know how they have done
+const h1 = document.querySelector("h1");
+if (percentage === 100) {
+  h1.innerText = `Wow! You Are A Music Nerd!`;
+} else if (percentage < 100 && percentage >= 90) {
+  h1.innerText = `Great Job!`;
+}
